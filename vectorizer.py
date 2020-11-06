fcca3a1c9f91436756668f2e520190c9591dd806
davletovb <43503037+davletovb@users.noreply.github.com>
2020-11-05 21:29:56 -0500

    add vectorizers, get average of vectors

--- vectorizer.py
+++ vectorizer.py
@@ -1,9 +1,29 @@
+import numpy as np
 import tensorflow_hub as hub
+import spacy
+from sklearn.feature_extraction.text import TfidfVectorizer
+
+nlp = spacy.load("en_core_web_lg")
 
 embed = hub.load("https://tfhub.dev/google/universal-sentence-encoder-large/5")
 
-tweets = tweet_df['cleaned'].tolist()
+def get_mean_tfidf(tokens):
+    word2vec = {}
+    word2weight = {}
+    tfidf = TfidfVectorizer()
+    all_words = set(word for word in tokens)
 
-meanvec = np.mean([embed([tweet]) for tweet in tweets], axis=0)
+    tfidf.fit(tokens)
+    word2weight = dict(zip(tfidf.get_feature_names(), tfidf.idf_))
 
-print(meanvec)
+    for word in all_words:
+        word2vec[word] = nlp(word).vector
+
+    mean_vec_tfidf = np.array([np.mean([word2vec[w] * word2weight[w] for w in tokens], axis=0)])
+    return mean_vec_tfidf
+
+def get_mean_transformer(corpus):
+    vectors = [embed([tweet]) for tweet in corpus]
+    vectors = np.array(vectors)
+    mean_vec_transformer = np.array([np.mean(vectors, axis=0)])
+    return mean_vec_transformer
