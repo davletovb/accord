43ad9f88834db7e10125bc10b8153385257b9a45
davletovb <43503037+davletovb@users.noreply.github.com>
2020-11-13 20:31:09 -0500

    get cleaned and tokenized data if doesn't exist

--- get_twitter_data.py
+++ get_twitter_data.py
@@ -1,10 +1,13 @@
 import re
 import sys
+from multiprocessing.pool import ThreadPool
+from os import path
 
 import pandas as pd
+import preprocessor as prep
 import tweepy
 
-from multiprocessing.pool import ThreadPool
+import get_tokens
 
 pool = ThreadPool(processes=2)
 
@@ -17,6 +20,26 @@ api = tweepy.API(auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True)
 if not api:
     print("Can't Authenticate")
     sys.exit(-1)
+
+
+def get_user(userid):
+    # user = api.get_user(userid)
+
+    user = pool.apply_async(api.get_user, (userid,))
+    pool.apply_async(pre_process, (userid,))
+
+    return user.get()
+
+
+def pre_process(userid):
+    if path.exists('data/' + userid + '.json'):
+        tweet_df = pd.read_json('data/' + userid + '.json')
+        return tweet_df
+    else:
+        tweet_df = get_tweets(userid)
+        cleaned_tweet_df = get_clean(tweet_df)
+        cleaned_tweet_df.to_json('data/' + userid + '.json')
+        return cleaned_tweet_df
 
 
 def get_tweets(userid, max_tweets=1000):
@@ -41,11 +64,44 @@ def get_tweets(userid, max_tweets=1000):
     return tweet_df
 
 
-def get_user(userid):
-    #user = api.get_user(userid)
+def get_clean(tweet_df):
+    punctuations = '''!()-=—!→–[]|{};:+`"“”\,<>/@#$%^&*_~'''
 
-    async_result = pool.apply_async(api.get_user, (userid,))
-    async_result2 = pool.apply_async(get_tweets, (userid,))
+    prep.set_options(prep.OPT.URL, prep.OPT.MENTION)
+
+    cleaned = [prep.clean(text) for text in tweet_df['text']]
+
+    tweet_df['cleaned'] = cleaned
+    tweet_df['cleaned'] = tweet_df.cleaned.apply(
+        lambda x: x.translate(str.maketrans(punctuations, ' ' * len(punctuations))))
+    tweet_df['cleaned'] = tweet_df.cleaned.apply(lambda x: remove_emojis(x))
+    tweet_df['cleaned'] = tweet_df.cleaned.apply(lambda x: x.replace('...', ' '))
+    tweet_df['cleaned'] = tweet_df.cleaned.apply(lambda x: ' '.join(x.split()))
+    tokens = [get_tokens.get_tokens(tweet) for tweet in tweet_df['cleaned']]
+    tweet_df['tokens'] = tokens
+
+    return tweet_df
 
 
-    return async_result.get()
+def remove_emojis(data):
+    emoj = re.compile("["
+                      u"\U0001F600-\U0001F64F"  # emoticons
+                      u"\U0001F300-\U0001F5FF"  # symbols & pictographs
+                      u"\U0001F680-\U0001F6FF"  # transport & map symbols
+                      u"\U0001F1E0-\U0001F1FF"  # flags (iOS)
+                      u"\U00002500-\U00002BEF"  # chinese char
+                      u"\U00002702-\U000027B0"
+                      u"\U00002702-\U000027B0"
+                      u"\U000024C2-\U0001F251"
+                      u"\U0001f926-\U0001f937"
+                      u"\U00010000-\U0010ffff"
+                      u"\u2640-\u2642"
+                      u"\u2600-\u2B55"
+                      u"\u200d"
+                      u"\u23cf"
+                      u"\u23e9"
+                      u"\u231a"
+                      u"\ufe0f"  # dingbats
+                      u"\u3030"
+                      "]+", re.UNICODE)
+    return re.sub(emoj, '', data)
