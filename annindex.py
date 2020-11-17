89b840fad8728440937475da738ebfe9db696ac7
davletovb <43503037+davletovb@users.noreply.github.com>
2020-11-16 23:07:29 -0500

    ANN index build and search methods

--- annindex.py
+++ annindex.py
@@ -1,37 +1,77 @@
-import annoy
-import faiss
+from annoy import AnnoyIndex
+import os
+import numpy as np
+import json
+#import faiss
 
-class AnnoyIndex():
-    def __init__(self, vectors, labels):
-        self.dimension = vectors.shape[1]
-        self.vectors = vectors.astype('float32')
-        self.labels = labels
+# class AnnoyIndex:
+#     def __init__(self, vectors, labels):
+#         self.dimension = vectors.shape[1]
+#         self.vectors = vectors.astype('float32')
+#         self.labels = labels
+#
+#     def build(self, number_of_trees=5):
+#         self.index = annoy.AnnoyIndex(self.dimension)
+#         for i, vec in enumerate(self.vectors):
+#             self.index.add_item(i, vec.tolist())
+#         self.index.build(number_of_trees)
+#
+#     def query(self, vector, k=10):
+#         indices = self.index.get_nns_by_vector(
+#             vector.tolist(),
+#             k,
+#             search_k=5)
+#         return [self.labels[i] for i in indices]
+#
+#
+# class LSHIndex:
+#     def __init__(self, vectors, labels):
+#         self.dimension = vectors.shape[1]
+#         self.vectors = vectors.astype('float32')
+#         self.labels = labels
+#
+#     def build(self, num_bits=8):
+#         self.index = faiss.IndexLSH(self.dimension, num_bits)
+#         self.index.add(self.vectors)
+#
+#     def query(self, vectors, k=10):
+#         distances, indices = self.index.search(vectors, k)
+#         # I expect only query on one vector thus the slice
+#         return [self.labels[i] for i in indices[0]]
 
-    def build(self, number_of_trees=10):
-        self.index = annoy.AnnoyIndex(self.dimension)
-        for i, vec in enumerate(self.vectors):
-            self.index.add_item(i, vec.tolist())
-        self.index.build(number_of_trees)
 
-    def query(self, vector, k=10):
-        indices = self.index.get_nns_by_vector(
-            vector.tolist(),
-            k,
-            search_k=search_in_x_trees)
-        return [self.labels[i] for i in indices]
+def build_index():
+    a = AnnoyIndex(512, 'angular')
+    index_map = {}
+    with os.scandir('vectors/') as entries:
+        for i, entry in enumerate(entries):
+            if (entry.name != "index.ann") and (entry.name != "index_map.json"):
+                vec = np.load('vectors/' + entry.name)
+                a.add_item(i, vec[0][0])
+                index_map[i] = entry.name
+
+    with open('vectors/index_map.json', 'w') as file:
+        json.dump(index_map, file, sort_keys=True, indent=4)
+
+    a.build(10)
+    a.save('vectors/index.ann')
+    print('ANN index built')
 
 
-class LSHIndex():
-    def __init__(self, vectors, labels):
-        self.dimension = vectors.shape[1]
-        self.vectors = vectors.astype('float32')
-        self.labels = labels
+def search_index(userid):
+    a = AnnoyIndex(512, 'angular')
+    with open('vectors/index_map.json', 'r') as file:
+        index_map = json.load(file)
 
-    def build(self, num_bits=8):
-        self.index = faiss.IndexLSH(self.dimension, num_bits)
-        self.index.add(self.vectors)
+    user_index = list(index_map.keys())[list(index_map.values()).index(userid + '.npy')]
+    a.load('vectors/index.ann')
+    neighbors = a.get_nns_by_item(int(user_index), 5)
+    users = [index_map[str(n)].replace('.npy', '.json') for n in neighbors]
+    result = []
 
-    def query(self, vectors, k=10):
-        distances, indices = self.index.search(vectors, k)
-        # I expect only query on one vector thus the slice
-        return [self.labels[i] for i in indices[0]]
+    for user in users:
+        with open('users/'+user, 'r') as file:
+            user_profile = json.load(file)
+        result.append(user_profile)
+    return result
+
