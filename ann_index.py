612f4e1ded91fdf382bbd97f7026f48bb8f1b7b6
davletovb <43503037+davletovb@users.noreply.github.com>
2020-11-18 20:44:10 -0500

    add ANN indexing for word based vectors

--- ann_index.py
+++ ann_index.py
@@ -40,31 +40,44 @@ import json
 #         return [self.labels[i] for i in indices[0]]
 
 
-def build_index():
-    a = AnnoyIndex(512, 'angular')
-    index_map = {}
-    with os.scandir('vectors/') as entries:
-        for i, entry in enumerate(entries):
-            if (entry.name != "index.ann") and (entry.name != "index_map.json"):
-                vec = np.load('vectors/' + entry.name)
-                a.add_item(i, vec[0][0])
-                index_map[i] = entry.name
+def build_index(sub):
+    if sub == 'word':
+        d = 300
+    else:
+        d = 512
 
-    with open('vectors/index_map.json', 'w') as file:
+    a = AnnoyIndex(d, 'angular')
+    index_map = {}
+
+    with os.scandir('vectors/'+sub+'/') as entries:
+        for i, entry in enumerate(entries):
+            #if (entry.name != "index.ann") and (entry.name != "index_map.json"):
+            vec = np.load('vectors/'+sub+'/' + entry.name)
+            print(entry.name)
+            print(vec)
+            a.add_item(i, vec[0])
+            index_map[i] = entry.name
+
+    with open('vectors/index_'+sub+'_map.json', 'w') as file:
         json.dump(index_map, file, sort_keys=True, indent=4)
 
     a.build(10)
-    a.save('vectors/index.ann')
+    a.save('vectors/index_'+sub+'.ann')
     print('ANN index built')
 
 
-def search_index(userid):
-    a = AnnoyIndex(512, 'angular')
-    with open('vectors/index_map.json', 'r') as file:
+def search_index(userid, sub):
+    if sub == 'word':
+        d = 300
+    else:
+        d = 512
+
+    a = AnnoyIndex(d, 'angular')
+    with open('vectors/index_'+sub+'_map.json', 'r') as file:
         index_map = json.load(file)
 
     user_index = list(index_map.keys())[list(index_map.values()).index(userid + '.npy')]
-    a.load('vectors/index.ann')
+    a.load('vectors/index_'+sub+'.ann')
     neighbors = a.get_nns_by_item(int(user_index), 5)
     users = [index_map[str(n)].replace('.npy', '.json') for n in neighbors]
     result = []
@@ -73,5 +86,5 @@ def search_index(userid):
         with open('users/'+user, 'r') as file:
             user_profile = json.load(file)
         result.append(user_profile)
-    return result
 
+    return result
